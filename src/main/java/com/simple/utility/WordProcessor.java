0492279d94c017268661d2a8dd01c396ab9e5169
alok104 <33246552+alok104@users.noreply.github.com>
2021-02-19 15:14:20 +0400

    Update WordProcessor.java

--- src/main/java/com/simple/utility/WordProcessor.java
+++ src/main/java/com/simple/utility/WordProcessor.java
@@ -7,7 +7,9 @@ import com.simple.constants.Constants;
 public class WordProcessor {
 
 	public static void buildWordCountMap(String line, Map<String, Integer> wordCountMap) {
-		for (String word : line.split(Constants.SPACE)) {
+	       if(wordCountMap == null)
+                   return;
+               for (String word : line.split(Constants.SPACE)) {
 			word = word.replaceAll("[^a-zA-Z0-9]+","");
 			if (wordCountMap.containsKey(word)) {
 				wordCountMap.put(word, wordCountMap.get(word) + 1);
